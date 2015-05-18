d24efa155c8a7687e460593e44ca7f9b13a11157
pstuart <pstuart2@gmail.com>
2015-05-18 15:49:47 -0500

    Fixing the build display script.

--- private/db/refactor-1.js
+++ private/db/refactor-1.js
@@ -3,7 +3,7 @@ db.getCollection('projects').update({}, {$rename: {'url': 'href'}}, {multi: true
 
 // builds: update url to href
 // builds: drop projectId, isDisplayed, currentBuild
-var builds = db.getCollections('builds');
+var builds = db.getCollection('builds');
 builds.update({}, {$rename: {'url': 'href'}, $unset: {projectId: '', isDisplayed: '', currentBuild: ''}, $set: {watchers: [], watcherCount: 0}}, {multi: true});
 
 // myBuildDisplay: add serverId
@@ -12,7 +12,7 @@ var myBuildDisplays = db.getCollection('myBuildDisplay');
 myBuildDisplays.update({}, {$set: {serverId: 'wtmyDGeknx9vW3k5A'}});
 
 // builds: Need to add watcherCount from myBuildDisplay
-myBuildDisplays.forEach(function (mbd) {
+myBuildDisplays.find({}).forEach(function (mbd) {
 	builds.update({_id: mbd.buildId}, {$inc: {watcherCount: 1}, $addToSet: {watchers: mbd.userId}});
 });
 
