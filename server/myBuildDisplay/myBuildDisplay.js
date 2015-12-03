905311aa3ceaf554c354f9ac2601e903bec83efe
pstuart <pstuart2@gmail.com>
2015-12-03 10:29:35 -0600

    No longer need those.

--- server/myBuildDisplay/myBuildDisplay.js
+++ server/myBuildDisplay/myBuildDisplay.js
@@ -1,13 +1,10 @@
 'use strict';
 Controllers.MyBuildDisplayAllow = (function () {
 	function Insert(userId, doc) {
-		// TODO: Get the count of the items and set the new sort to that.
-		//var count = Collections.MyBuildDisplay.find({userId: Meteor.userId()}).count();
 		return (userId === Meteor.userId());
 	}
 
 	function Update(userId, doc, fieldNames, modifier) {
-		// TODO: If there is a sort, then make sure to toggle with the old sort.
 		return (userId === Meteor.userId());
 	}
 
